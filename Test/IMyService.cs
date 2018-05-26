a7ed93230f4301de0ccb063211ff71b58912b2f7
ARTUR SEDRAKYAN <arthursedrakyan98@gmail.com>
2018-05-26 20:04:37 +0400

    Ok

---
+++ Test/IMyService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Test
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMyService
+    {
+        [OperationContract]
+        Result SorcePerson(Person person);
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "Test.ContractType".
+    [DataContract]
+    public class Person
+    {
+        private int id;
+        private string name;
+        private double price;
+        private string group;
+
+        [DataMember]
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DataMember]
+        public string Group
+        {
+            get { return group; }
+            set { group = value; }
+        }
+
+        [DataMember]
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+
+
+    [DataContract]
+    public struct Result
+    {
+        public Result(string messege, string answer)
+        {
+            this.messege = messege;
+            this.answer = answer;
+        }
+
+        private string messege;
+        private string answer;
+
+        [DataMember]
+        public string Messege
+        {
+            get { return messege; }
+            set { messege = value; }
+        }
+
+        [DataMember]
+        public string Answer
+        {
+            get { return answer; }
+            set { answer = value; }
+        }
+    }
+}
---
+++ Test/MyServiceImp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Test
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class MyServiceImp : IMyService
+    {
+        private static List<int> ides = new List<int>();
+        public Result SorcePerson(Person person)
+        {
+            if (ides.Count == 0)
+                GetIdes();
+
+            if (person.Name.Equals(""))
+                return new Result("there aren't Name", "Failed");
+
+            foreach (var item in ides)
+            {
+                if (item == person.ID)
+                    return new Result("The Person is already exist", "Failed");
+            }
+
+            string connectionString =
+            "Data Source=(local);Initial Catalog=MyWCFDataBasa;"
+            + "Integrated Security=true";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string queryString = $"INSERT INTO Humons (Id,[Name],[Group],Price)" +
+                    $" VALUES('{person.ID}','{person.Name}','{person.Group}','{person.Price}')";
+
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
+            ides.Add(person.ID);
+            return new Result("The Person will be added", "Access");
+        }
+
+        private void GetIdes()
+        {
+            string connectionString =
+             "Data Source=(local);Initial Catalog=MyWCFDataBasa;"
+             + "Integrated Security=true";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string queryString = "select Id from Humons";
+
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                SqlDataReader dataReader = command.ExecuteReader();
+                List<string> columns = new List<string>();
+                while (dataReader.Read())
+                {
+                    ides.Add(dataReader.GetInt32(0));
+                }
+                dataReader.Close();
+            }
+        }
+    }
+}
